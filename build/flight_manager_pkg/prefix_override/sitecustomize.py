89a3fc50ff1af79bb11237731cd9ccb10914ec2b
Yiğit YILMAZ <c2126073@student.cankaya.edu.tr>
2025-06-29 13:46:24 +0300

    Initial Commit

---
+++ build/flight_manager_pkg/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/yigit/tukan_ros2_ws/install/flight_manager_pkg'
---
+++ build/tracking_system_pkg/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/yigit/tukan_ros2_ws/install/tracking_system_pkg'
